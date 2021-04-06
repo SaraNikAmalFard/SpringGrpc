80257943ada2c7c098a0e765bdd6b98006e19a08
Sara NikAmalFard <sarah_nikamalfard@yahoo.com>
2021-04-06 20:04:21 +0100

    changes 4

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientController.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientController.java
@@ -6,11 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestController
 public class GRPCClientController {
@@ -19,13 +23,43 @@ public class GRPCClientController {
 	@Autowired
     	public GRPCClientController(GRPCClientService grpcClientService) {
         	this.grpcClientService = grpcClientService;
-    	}    
+    	}
+
+	@Autowired
+	public FileStorageService fileStorageService;
+
+
+	@PostMapping("/uploadFile")
+	public String uploadFile(@RequestParam("file") MultipartFile file)
+	{
+		fileStorageService.storeFile(file);
+		return "File successfully uploaded";
+	}
+
+	@PostMapping("/uploadMultipleFiles")
+	public String uploadMultipleFiles(@RequestParam("files") MultipartFile[] files)
+	{
+		Arrays.asList(files).stream().map(file->uploadFile(file)).collect(Collectors.toList());
+		if(grpcClientService.checkFiles() == true)
+		{
+			return "File successfully uploaded";
+		}
+		else
+		{
+			grpcClientService.delete();
+			return "The matrices or the file you uploaded was not accepted!";
+		}
+	}
 	@GetMapping("/ping")
     	public String ping() {
         	return grpcClientService.ping();
     	}
 
     @GetMapping("/getFootprint")
-      public double getFootprint() {return grpcClientService.getFootprint();}
-
+      public double getFootprint()
+	  {
+	  	System.out.println("Creating 2D array from the file");
+	  	grpcClientService.create2DArrayFromFile();
+	  	return grpcClientService.getFootprint();
+	  }
 }
--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -6,6 +6,13 @@ import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 @Service
 public class GRPCClientService {
     public String ping() {
@@ -21,27 +28,128 @@ public class GRPCClientService {
 		return helloResponse.getPong();
     }
 
+	public static int matrixA[][];
+	public static int matrixB[][];
 
-	/*public static double getFootprint()
+	public boolean isPowerOfTwo(int n)
 	{
-		int[][] matrixOne = {{1,2} , {3,4}};
-		int[][] matrixTwo = {{5,6} , {7,8}};
-		long startTime = System.nanoTime();
+		return (int) (Math.ceil((Math.log(n) / Math.log(2))))
+				== (int) (Math.floor(((Math.log(n) / Math.log(2)))));
+	}
 
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("172.31.74.179", 8081)
-				.usePlaintext()
-				.build();
-		MatrixMultiplicationServiceGrpc.MatrixMultiplicationServiceBlockingStub stub
-				= MatrixMultiplicationServiceGrpc.newBlockingStub(channel);
 
-		stub.multiplyBlock(requestBuilder(matrixOne,matrixTwo));
-		System.out.println(" Foot print is called");
+	public boolean checkFiles()
+	{
+		File directoryPath = new File("./uploads");
+		String[] contents = directoryPath.list();
+		String filePath = null;
+		List<String> lines = new ArrayList();
 
-		long endTime = System.nanoTime();
-		long footPrint = endTime - startTime;
-		double ftprnt = (double) footPrint / 1_000_000_000;
-		return ftprnt;
-	}*/
+		System.out.println("Checking the files path ... ");
+
+		int commonLineSize = 0;
+
+		if (contents != null)
+		{
+			System.out.println("File path is not empty!");
+			filePath = "./uploads" + "/" + contents[0];
+			FileInputStream fileInputStream = null;
+			try {
+				fileInputStream = new FileInputStream(filePath);
+				Scanner scanner = new Scanner(fileInputStream);
+				while (scanner.hasNextLine()) {
+					lines.add(scanner.nextLine());
+				}
+				scanner.close();
+
+				if (isPowerOfTwo(lines.size()) == true) {
+					String[] elements;
+					for (String line : lines) {
+						elements = line.split(" ");
+						if (elements.length != lines.size()) {
+							return false;
+						}
+					}
+				} else {
+					return false;
+				}
+			} catch (java.io.FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			filePath = "./uploads" + "/" + contents[1];
+			try {
+				commonLineSize = lines.size();
+				lines.clear();
+				fileInputStream = new FileInputStream(filePath);
+				Scanner scanner = new Scanner(fileInputStream);
+				while (scanner.hasNextLine()) {
+					lines.add(scanner.nextLine());
+				}
+				scanner.close();
+
+				if (lines.size() == commonLineSize) {
+					String[] elements;
+					for (String line : lines) {
+						elements = line.split(" ");
+						if (elements.length != commonLineSize) {
+							return false;
+						}
+					}
+				} else {
+					return false;
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			return true;
+		}
+		return false;
+	}
+
+	public static void delete()
+	{
+		File directory = new File("./uploads");
+		File[] files = directory.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				file.delete();
+			}
+		}
+	}
+
+	public void create2DArrayFromFile() {
+		File directoryPath = new File("./uploads");
+		String[] contents = directoryPath.list();
+		String lineA;
+		String lineB;
+		int row = 0;
+		int size = 0;
+		System.out.println("Creating 2D matrix A and B");
+		try {
+			BufferedReader bufferA = new BufferedReader(new FileReader("./uploads" + "/" + contents[0]));
+			BufferedReader bufferB = new BufferedReader(new FileReader("./uploads" + "/" + contents[1]));
+			while ((lineA = bufferA.readLine()) != null) {
+				lineB = bufferB.readLine();
+				String[] valsA = lineA.trim().split("\\s+");
+				String[] valsB = lineB.trim().split("\\s+");
+				if (matrixA == null) {
+					size = valsA.length;
+					matrixA = new int[size][size];
+					matrixB = new int[size][size];
+				}
+
+				for (int col = 0; col < size; col++) {
+					matrixA[row][col] = Integer.parseInt(valsA[col]);
+					matrixB[row][col] = Integer.parseInt(valsB[col]);
+				}
+				row++;
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
 
 
 	public /*static*/ MatrixMultiplicationRequest requestBuilder(int[][] matrixA, int[][] matrixB)
@@ -69,6 +177,8 @@ public class GRPCClientService {
 	public /*static*/ double getFootprint()
 	{
 		System.out.println("Entered footprint method");
+		System.out.println("Matrix A size = "+ matrixA.length);
+		System.out.println("First element of matrix B is =" + matrixB[0][0]);
 		int[][] matrixOne = {{1,2} , {3,4}};
 		int[][] matrixTwo = {{5,6} , {7,8}};
 		long startTime = System.nanoTime();
@@ -82,12 +192,12 @@ public class GRPCClientService {
 
 		stub.multiplyBlock(requestBuilder(matrixOne,matrixTwo));
 
-		System.out.println(" Foot print is called ... ");
+		System.out.println("Foot print is called ... ");
 
 		long endTime = System.nanoTime();
 		long footPrint = endTime - startTime;
 		double ftprnt = (double) footPrint / 1_000_000_000;
-		System.out.println("Foor print is = " + ftprnt);
+		System.out.println("Foot print is = " + ftprnt);
 		return ftprnt;
 	}
 }
